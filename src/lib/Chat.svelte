b7f75b26a683d0abe833cad90297b0507af6bae1
Giammaria Biffi <gmb2005itabackup@gmail.com>
2023-07-25 17:54:07 +0200

    Added writing bar

--- src/lib/Chat.svelte
+++ src/lib/Chat.svelte
@@ -1,7 +1,21 @@
-<script lang="ts"></script>
+<script lang="ts">
+    import WritingBar from "./WritingBar.svelte";
+</script>
 
 <div class="chat-div">
 <div>
+
+    <WritingBar></WritingBar>
     <!--Component della barra di scrittura-->
 </div>
 </div>
+
+<style lang="scss">
+    .chat-div{
+        div{
+            display: flex;
+            flex-direction: column;
+            justify-content: flex-end;
+        }
+    }
+</style>
---
+++ src/lib/WritingBar.svelte
@@ -0,0 +1,9 @@
+<script lang="ts">
+let message = "";
+</script>
+
+
+<div class="writing-div">
+    <input type="text" placeholder="Scrivi un messaggio" bind:value={message}/>
+    <button>Invia</button>
+</div>
