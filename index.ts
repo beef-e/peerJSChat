7f79c18751151d46ea5ebc7ad3e5ee90c86a60dd
Giammaria Biffi <gmb2005itabackup@gmail.com>
2023-07-28 23:35:19 +0200

    improved header and (useless) code

--- index.ts
+++ index.ts
@@ -1,7 +1,41 @@
 import { Peer } from 'peerjs';
+import { writable } from 'svelte/store';
+import { setContext } from 'svelte';
+//import { otherID } from 'src/lib/Header.svelte'
+let otherID = writable('');
+export { otherID };
 
 const peer = new Peer();
 
+let peerID: Promise<string> = Promise.resolve(null);
+
 peer.on('open', (id) => {
+	/*return new Promise((resolve, reject) => {
+		peerID = id;
+		resolve(id);
+	});*/
+
+	peerID = Promise.resolve(id);
+	return peerID;
 	console.log('My peer ID is: ' + id);
 });
+
+console.log('il mio peerID: ' + peerID);
+
+try {
+	peer.connect(otherID.toString());
+} catch (error) {
+	console.log('errore connessione');
+	console.log(error);
+}
+
+try {
+	peer.on('connection', (conn) => {
+		console.log('connessione stabilita');
+	});
+} catch (error) {
+	console.log('errore connessione seconod try');
+	console.log(error);
+}
+
+export { peerID };
--- src/lib/Header.svelte
+++ src/lib/Header.svelte
@@ -1,4 +1,31 @@
-<script lang="ts"></script>
+<script>
+import { onMount } from "svelte";
+import {peerID} from "../.."
+import {otherID} from "../.."
+import { writable } from "svelte/store";
+//let myPeerID: string;
+let isPeerIDSet = false;
+let peerIDString;
+let otherIDString=writable("");
+otherIDString.subscribe(value => {
+    // @ts-ignore
+    otherIDString=value;
+});
+
+onMount(async () => {
+    peerID.then(()=>{
+        peerIDString=peerID.toString();
+        isPeerIDSet = true;
+    })
+});
+
+function trasferisciOtherID() {
+    console.log("trasferisco " + otherIDString);
+    // @ts-ignore
+    otherID.set(otherIDString);
+}
+
+</script>
 
 <header>
     <h1>PeerChat</h1>
@@ -6,7 +33,10 @@
     <nav>
         <ul>
             <li><a href="https://github.com/beef-e/peerJSChat">About</a></li>
-            <li><a href="#">My ID</a></li>
+            {#if isPeerIDSet}
+            <li><a href="#">My ID: {peerIDString}</a></li>
+            {/if}
+            <li><input type="text" name="otherID" id="otherID" placeholder="Insert another ID" bind:value={otherIDString}><button on:click={trasferisciOtherID}>Connect</button></li>
         </ul>
     </nav>
 </header>
