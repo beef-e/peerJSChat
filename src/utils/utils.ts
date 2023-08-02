4a26ba08c91b91503f4e57af42c6d35dc08b08ac
Giammaria Biffi <gmb2005itabackup@gmail.com>
2023-08-02 22:58:21 +0200

    updated debugging helpers

--- src/utils/utils.ts
+++ src/utils/utils.ts
@@ -14,7 +14,12 @@ export let testo: string;
 export const peer = new Peer();
 
 peer.on('connection', (connection) => {
+	console.log('I am in peer.on(connection)');
 	conn = connection;
+	console.log(connection);
+	console.log(conn);
+	console.log('Connected passively to: ' + conn.peer);
+	console.log('conn.open è: ' + conn.open);
 });
 
 destID.subscribe((value) => {
@@ -29,14 +34,22 @@ MyID.subscribe((value) => {
 message.subscribe((value) => {
 	testo = value;
 
-	if (testo.length > 0) {
-		conn.send(testo);
+	if (testo.length > 0 && conn != null) {
+		try {
+			conn.send(testo);
+		} catch (error) {
+			console.log('Errore: ' + error);
+		}
 		console.log('Sent: ', testo);
 	}
 });
 
 conn.on('open', () => {
 	console.log('Connected');
+
+	if (conn.open) {
+		console.log('conn.open è true');
+	}
 });
 
 conn.on('data', (data) => {
