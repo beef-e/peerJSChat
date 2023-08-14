497675cbc8830bacbc38f71b8c8c0863d1d24212
Giammaria Biffi <102854627+beef-e@users.noreply.github.com>
2023-08-14 15:27:24 +0200

    Cleaning and reformatting code

--- src/lib/Header.svelte
+++ src/lib/Header.svelte
@@ -1,43 +1,52 @@
 <script>
-import { onMount } from "svelte";
-import {peer} from "../..";
-import { destID, MyID } from "../utils/utils";
-let otherID;
-let fastID;
-function connectPeer(){
-    destID.set(otherID);
-    console.log("Connected to " + $destID);
-    
-};
+	import { onMount } from 'svelte';
+	import { peer } from '../..';
+	import { destID, MyID } from '../utils/utils';
+	let otherID;
+	let fastID;
+	function connectPeer() {
+		destID.set(otherID);
+		console.log('Connected to ' + $destID);
+	}
 
-onMount(async () => {
-    await setTimeout(() => {}, 1500);
-});
+	onMount(async () => {
+		await setTimeout(() => {}, 1500);
+	});
 
-function handleClick() {
-    navigator.clipboard.writeText(peer.id);
-    alert("Copied to clipboard!");
-    MyID.set(peer.id);
-    fastID = peer.id;
-}
+	function handleClick() {
+		navigator.clipboard.writeText(peer.id);
+		alert('Copied to clipboard!');
+		MyID.set(peer.id);
+		fastID = peer.id;
+	}
 </script>
 
 <header>
-    <h1>PeerChat</h1>
+	<h1>PeerChat</h1>
 
-    <nav>
-        <ul>
-            <li class="FirstChild"><a href="https://github.com/beef-e/peerJSChat">About</a></li>
-            
-            {#await peer.id||fastID then }
-                {#if peer.id || fastID}
-                    <li><span on:click={handleClick}>My ID: {peer.id || fastID}</span></li>
-                    {:else}
-                    <li><span on:click={handleClick}>My ID: Loading...</span></li>
-                {/if}
-            {/await}
+	<nav>
+		<ul>
+			<li class="FirstChild"><a href="https://github.com/beef-e/peerJSChat">About</a></li>
 
-            <li class="lastChild"><form on:submit|preventDefault={connectPeer}><input type="text" name="otherID" id="otherID" placeholder="Insert another ID" bind:value={otherID}><button type="submit">Connect</button></form></li>
-        </ul>
-    </nav>
+			{#await peer.id || fastID then}
+				{#if peer.id || fastID}
+					<li><span on:click={handleClick}>My ID: {peer.id || fastID}</span></li>
+				{:else}
+					<li><span on:click={handleClick}>My ID: Loading...</span></li>
+				{/if}
+			{/await}
+
+			<li class="lastChild">
+				<form on:submit|preventDefault={connectPeer}>
+					<input
+						type="text"
+						name="otherID"
+						id="otherID"
+						placeholder="Insert another ID"
+						bind:value={otherID}
+					/><button type="submit">Connect</button>
+				</form>
+			</li>
+		</ul>
+	</nav>
 </header>
